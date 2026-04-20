d3f5c4e6edae0fd07a91439573b5776a6d539977
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-20 10:26:50 +0700

    membuat otput nilai

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -9,6 +9,7 @@ class Mahasiswa{
 
     void printData(){
         cout << "NIM" << nim << endl;
-         cout << "Nama" << nama << endl;
+        cout << "Nama" << nama << endl;
+        cout << "Nilai" << nilai << endl;
     }
 }
